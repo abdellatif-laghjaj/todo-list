98c889db4414b39192d445ac2dde8ad6acd1fde5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-04 20:57:58 +0100

    error message

--- js/main.js
+++ js/main.js
@@ -8,8 +8,37 @@ const edit_btns = document.querySelectorAll('.edit-btn');
 
 
 
+let todos = [];
 
 
+function addToDo(task_input) {
+    let task = {
+        id: todos.length + 1,
+        task: task_input.value,
+        completed: false
+    }
+    todos.push(task);
+}
+
+add_btn.addEventListener('click', () => {
+    if (task_input.value === '') {
+        showAlertMessage('Please enter a task');
+    }else {
+        addToDo(task_input);
+    }
+});
+
+
+//show alert message
+function showAlertMessage(message) {
+    alert_message.innerHTML = message;
+    alert_message.classList.remove('hide');
+    alert_message.classList.add('show');
+    setTimeout(() => {
+        alert_message.classList.remove('show');
+        alert_message.classList.add('hide');
+    }, e000);
+}
 
 //check if todos are empty
 if (todos.length === 0) {
